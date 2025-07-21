0e1a3b72520645bd56c3ff96af48640e6efb27dd
Kumaranraja <kumaranraja46248@gmail.com>
2025-07-21 23:33:54 +0530

    #29 json

--- frontend/react/codexsun/src/AppRoutes.tsx
+++ frontend/react/codexsun/src/AppRoutes.tsx
@@ -1,63 +1,3 @@
-// // import { Routes, Route } from "react-router-dom";
-// // import Login from "./pages/auth/Login";
-// // import SignUp from "./pages/auth/Signup";
-// // import NotFound from "./Components/NotFound";
-// // import { AuthProvider } from "./pages/auth/AuthContext";
-
-// // import "animate.css";
-// // import ProtectedRoute from "./pages/auth/ProtectedRoute";
-// // import Admin from "./pages/app/codexsun/Admin";
-
-// // export default function AppRoutes() {
-// //   return (
-// //     <AuthProvider>
-// //       <Routes>
-// //         <Route path="/" element={<Login />} />
-// //         <Route path="/signup" element={<SignUp />} />
-
-// //         <Route
-// //           path="/dashboard/:component?"
-// //           element={
-// //             <ProtectedRoute>
-// //               <Admin />
-// //             </ProtectedRoute>
-// //           }
-// //         />
-
-// //         <Route path="*" element={<NotFound />} />
-// //       </Routes>
-// //     </AuthProvider>
-// //   );
-// // }
-
-
-// // src/routes/index.tsx
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useAppContext } from "./pages/GlobalContext/AppContaxt";
-// import Codexsun from './pages/app/codexsun/route'
-// import Cortex from './pages/app/cortex/route'
-// const AppRouter = () => {
-//   const { APP_CODE } = useAppContext(); // e.g., 'ecart', 'crm', 'lms'
-
-//   const getRoutes = () => {
-//     switch (APP_CODE) {
-//       case "ecart":
-//         return <Codexsun />;
-//       case "crm":
-//         return <Cortex />;
-//       default:
-//         return <div>App Not Found</div>;
-//     }
-//   };
-
-//   return (
-//     <BrowserRouter>
-//       <Routes>{getRoutes()}</Routes>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default AppRouter;
 
 // src/AppRouter.tsx
 import { BrowserRouter } from "react-router-dom";
@@ -68,7 +8,7 @@ import Cortex from "./pages/app/cortex/route";
 
 const AppRoutes = () => {
   const { APP_CODE } = useAppContext();
-
+console.log("type",APP_CODE)
   const routes = (() => {
     switch (APP_CODE) {
       case "web":
--- frontend/react/codexsun/src/main.tsx
+++ frontend/react/codexsun/src/main.tsx
@@ -1,20 +1,20 @@
-// main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./theme.css";
 import AppRouter from "./AppRoutes";
-import { AppProvider } from "./pages/GlobalContext/AppContaxt";
-import AppInitializer from "./pages/app/useSettings";
 import { AuthProvider } from "./pages/app/auth/AuthContext";
+import AppInitializer from "./pages/app/useSettings";
+import { AppProvider } from "./pages/GlobalContext/AppContaxt";
+import settings from "../public/settings.json"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppProvider>
-      <AuthProvider>
-        <AppInitializer> {/* ✅ Wrap with this */}
-            <AppRouter />
-        </AppInitializer>
-      </AuthProvider>
+    <AppProvider initialSettings={settings}> {/* ✅ wrap at the top */}
+      <AppInitializer>
+        <AuthProvider>
+          <AppRouter />
+        </AuthProvider>
+      </AppInitializer>
     </AppProvider>
   </React.StrictMode>
 );
