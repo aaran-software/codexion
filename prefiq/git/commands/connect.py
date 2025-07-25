d33357a744c8e0e61f47f4a0d6e2127f83f6076a
SUNDAR <sundar@codexsun.com>
2025-07-25 08:31:22 +0530

    #61 refactor work on prefiq

--- prefiq/git/commands/connect.py
+++
@@ -1,8 +0,0 @@
-import typer
-from prefiq.commands.utils.ui import print_success
-
-
-def run(
-    app: str = typer.Argument(..., help="App name")
-):
-    print_success("[OK] SSH key connected (mocked)")
--- prefiq/git/commands/status.py
+++
@@ -1,8 +0,0 @@
-import typer
-from prefiq.commands.utils.ui import print_success
-
-
-def run(
-    app: str = typer.Argument(..., help="App name")
-):
-    print_success(f"[OK] Status for {app}: clean (mocked)")
