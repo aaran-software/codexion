b668ade412463b5f49fe67acf0ccb8e2d18c9513
SUNDAR <sundar@codexsun.com>
2025-07-29 11:14:27 +0530

    #101 working on app Dockerfile

--- cortex/main.py
+++ cortex/main.py
@@ -42,7 +42,7 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=4000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=4001, reload=True)
 
     for route in app.routes:
         print(route.path)
